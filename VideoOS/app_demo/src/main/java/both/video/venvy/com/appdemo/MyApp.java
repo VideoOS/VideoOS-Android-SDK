ba21a130664ee67951ee31f4abffbb4c8a253fa1
androidlgf <android_lgf@163.com>
2019-04-01 17:46:17 +0800

    add Application appCreateSAAS

--- VideoOS/app_demo/src/main/java/both/video/venvy/com/appdemo/MyApp.java
+++ VideoOS/app_demo/src/main/java/both/video/venvy/com/appdemo/MyApp.java
@@ -9,11 +9,16 @@ import cn.com.videopls.pub.VideoPlus;
 
 import android.support.multidex.MultiDex;
 
+import org.luaj.vm2.ast.Str;
+
 /**
  * Created by lgf on 2017/3/22.
  */
 
 public class MyApp extends Application {
+    private final String appKey = "e3095ad4-5927-40eb-b6e5-a43b7f1e966b";
+    private final String appSecret = "b28a1f82e6c147d8";
+
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
@@ -25,6 +30,6 @@ public class MyApp extends Application {
         super.onCreate();
         DebugStatus.changeEnvironmentStatus(DebugStatus.EnvironmentStatus.RELEASE);
         VenvyLog.needLog = true;
-        VideoPlus.appCreateSAAS(MyApp.this,"e3095ad4-5927-40eb-b6e5-a43b7f1e966b");
+        VideoPlus.appCreateSAAS(MyApp.this, appKey, appSecret);
     }
 }
