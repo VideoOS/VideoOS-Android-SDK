09936b2f988e158aefbe9e79977955e4711cd944
androidlgf <android_lgf@163.com>
2019-05-27 17:24:02 +0800

    change VideoPlayerSize

--- VideoOS/VenvyLibrary/src/main/java/cn/com/venvy/common/bean/VideoPlayerSize.java
+++ VideoOS/VenvyLibrary/src/main/java/cn/com/venvy/common/bean/VideoPlayerSize.java
@@ -12,6 +12,9 @@ public class VideoPlayerSize {
     public int mVerVideoWidth;
     public int mPortraitSmallScreenOriginY; //竖屏异形屏幕下离顶部的高度，例如iphoneX 顶部title栏大小
 
+    public int mFullScreenContentWidth;
+    public int mFullScreenContentHeight;
+
     public VideoPlayerSize() {
 
     }
@@ -22,5 +25,20 @@ public class VideoPlayerSize {
         this.mVerVideoWidth = verVideoWidth;
         this.mVerVideoHeight = verVideoHeight;
         this.mPortraitSmallScreenOriginY = portraitSmallScreenOriginY;
+    }
+
+    /***
+     * fullScreenContentWidth,fullScreenContentHeight为界面全屏可展示的最大宽高
+     *
+     * @param fullScreenContentWidth 横竖屏内容区宽度
+     * @param fullScreenContentHeight 横竖屏内容区高度
+     * @param verVideoWidth  播放器竖屏宽度
+     * @param verVideoHeight 播放器竖屏高度
+     */
+    public VideoPlayerSize(int fullScreenContentWidth, int fullScreenContentHeight, int verVideoWidth, int verVideoHeight) {
+        this.mFullScreenContentWidth = fullScreenContentWidth;
+        this.mFullScreenContentHeight = fullScreenContentHeight;
+        this.mVerVideoWidth = verVideoWidth;
+        this.mVerVideoHeight = verVideoHeight;
     }
 }
