c95c21fe47b1b745e2974f9ec2b248f7552ba350
androidlgf <android_lgf@163.com>
2019-06-05 11:07:49 +0800

    fix OkHttpClient protocols

--- VideoOS/VenvyLibrary/src/main/java/cn/com/venvy/common/okhttp/OkhttpClientUtil.java
+++ VideoOS/VenvyLibrary/src/main/java/cn/com/venvy/common/okhttp/OkhttpClientUtil.java
@@ -28,7 +28,7 @@ public class OkhttpClientUtil {
             synchronized (OkhttpClientUtil.class) {
                 if (client == null) {
                     HttpSSLParams.SSLParams sslParams = initSSLParams(context);
-                    client = new OkHttpClient.Builder().protocols(Arrays.asList(Protocol.HTTP_1_1, Protocol.HTTP_2))
+                    client = new OkHttpClient.Builder().protocols(Arrays.asList(Protocol.HTTP_1_1))
                             .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
                             .dispatcher(new Dispatcher(new ThreadPoolExecutor(0, 100, 60, TimeUnit.SECONDS,
                                     new SynchronousQueue<Runnable>(), Util.threadFactory("OkHttp Dispatcher", false))))
