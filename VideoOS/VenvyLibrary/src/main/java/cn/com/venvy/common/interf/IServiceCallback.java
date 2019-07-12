0136475f0b284e2d92032527efb460bb00c387ac
androidlgf <android_lgf@163.com>
2019-07-12 15:03:58 +0800

    add 视联网回调 IServiceCallback

---
+++ VideoOS/VenvyLibrary/src/main/java/cn/com/venvy/common/interf/IServiceCallback.java
@@ -0,0 +1,14 @@
+package cn.com.venvy.common.interf;
+
+/**
+ * Created by videojj_pls on 2019/7/12.
+ * 视联网模式事件回调
+ */
+
+public interface IServiceCallback {
+    void onCompleteForService();
+
+    void onFailToCompleteForService(Exception e);
+
+    void onClickForService(ServiceType type, String eventType);
+}
