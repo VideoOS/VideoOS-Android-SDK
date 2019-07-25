fa5c545a68607041849b7f5326c98d725959ed11
androidlgf <android_lgf@163.com>
2019-07-25 16:31:37 +0800

    VenvyMD5Util add EncoderByMd5

--- VideoOS/VenvyLibrary/src/main/java/cn/com/venvy/common/utils/VenvyMD5Util.java
+++ VideoOS/VenvyLibrary/src/main/java/cn/com/venvy/common/utils/VenvyMD5Util.java
@@ -93,4 +93,29 @@ public class VenvyMD5Util {
 
         return strResult;
     }
+
+    public static String EncoderByMd5(String buf) {
+        try {
+            MessageDigest digist = MessageDigest.getInstance("MD5");
+            byte[] rs = digist.digest(buf.getBytes("UTF-8"));
+            StringBuffer digestHexStr = new StringBuffer();
+            for (int i = 0; i < 16; i++) {
+                digestHexStr.append(byteHEX(rs[i]));
+            }
+            return digestHexStr.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public static String byteHEX(byte ib) {
+        char[] Digit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        char[] ob = new char[2];
+        ob[0] = Digit[(ib >>> 4) & 0X0F];
+        ob[1] = Digit[ib & 0X0F];
+        String s = new String(ob);
+        return s;
+    }
 }
