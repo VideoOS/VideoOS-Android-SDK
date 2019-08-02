cb85a19e8f4dbb07a65e49648e3eef67b554aa9d
androidlgf <android_lgf@163.com>
2019-08-02 10:16:12 +0800

    change demo

--- VideoOS/app_demo/src/main/java/both/video/venvy/com/appdemo/MyApp.java
+++ VideoOS/app_demo/src/main/java/both/video/venvy/com/appdemo/MyApp.java
@@ -31,7 +31,7 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
         appContext = this;
-        DebugStatus.changeEnvironmentStatus(DebugStatus.EnvironmentStatus.PREVIEW);
+        DebugStatus.changeEnvironmentStatus(DebugStatus.EnvironmentStatus.RELEASE);
         VenvyLog.needLog = true;
         ConfigUtil.putAppKey(ConfigUtil.RELEASE_APP_KEY);
         ConfigUtil.putAppSecret(ConfigUtil.RELEASE_APP_SECRET);
