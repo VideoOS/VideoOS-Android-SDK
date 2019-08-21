6b3e76e287c33b45054b102d57fc3ea9d6789879
androidlgf <android_lgf@163.com>
2019-08-21 14:03:43 +0800

    remove无效的注释

--- VideoOS/venvy_pub/src/main/java/cn/com/videopls/pub/view/VideoOSLuaView.java
+++ VideoOS/venvy_pub/src/main/java/cn/com/videopls/pub/view/VideoOSLuaView.java
@@ -198,10 +198,6 @@ public class VideoOSLuaView extends VideoOSBaseView {
         if (TextUtils.isEmpty(luaName)) {
             return;
         }
-//        if(luaName.contains("os_vote")){
-//            runLua(luaView, luaName, valueData);
-//            return;
-//        }
         if (sScriptBundle == null) {
             VenvyAsyncTaskUtil.doAsyncTask(INIT_SCRIPT,
                     new VenvyAsyncTaskUtil.IDoAsyncTask<Object, ScriptBundle>() {
