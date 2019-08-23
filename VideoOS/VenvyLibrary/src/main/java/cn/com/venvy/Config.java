4bc1a751f3c9a172072844d2bc5661992b9b889a
androidlgf <android_lgf@163.com>
2019-08-23 15:13:07 +0800

    update Config SDK_VERSION

--- VideoOS/VenvyLibrary/src/main/java/cn/com/venvy/Config.java
+++ VideoOS/VenvyLibrary/src/main/java/cn/com/venvy/Config.java
@@ -6,7 +6,7 @@ package cn.com.venvy;
  */
 
 public class Config {
-    public static String SDK_VERSION = "2.0.0";
+    public static String SDK_VERSION = "2.0.1";
     public static String HOST_VIDEO_OS = "https://os-saas.videojj.com/os-api-saas";
     public static boolean REPORT_ABLE = true;
     public static int DEBUG_STATUS = 0;
