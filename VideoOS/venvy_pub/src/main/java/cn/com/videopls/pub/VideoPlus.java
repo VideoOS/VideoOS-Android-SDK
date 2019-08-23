26daf26d1fba54123e111e12c553937a7ac238e1
androidlgf <android_lgf@163.com>
2019-08-23 15:00:55 +0800

    remove VideoPlusLuaUpdateModel

--- VideoOS/venvy_pub/src/main/java/cn/com/videopls/pub/VideoPlus.java
+++ VideoOS/venvy_pub/src/main/java/cn/com/videopls/pub/VideoPlus.java
@@ -18,34 +18,6 @@ public class VideoPlus {
 
     private static boolean executed = false;
 
-//    public static void appCreate(final Context context) {
-//        App.setContext(context);
-//        Config.SDK_VERSION = BuildConfig.SDK_VERSION;
-//        Config.REPORT_ABLE = BuildConfig.ReportAble;
-//        Config.DEBUG_STATUS = BuildConfig.DebugStatus;
-//
-//        if (executed) {
-//            return;
-//        }
-//        executed = true;
-//        LuaHelper.initLuaConfig();
-//        PlatformInfo platformInfo = new PlatformInfo.Builder().builder();
-//        Platform platform = new Platform(platformInfo);
-//        new VideoPlusLuaUpdateModel(platform, null).startRequest();
-//        VenvyRouterManager.getInstance().init(context, DebugStatus.isRelease() ? null : new VenvyRouterManager.RouterInitResult() {
-//            @Override
-//            public void initSuccess() {
-//                VenvyLog.d(VideoPlus.class.getName(), "VenvyRouter init success");
-//            }
-//
-//            @Override
-//            public void initFailed() {
-//
-//            }
-//        });
-//        new DownloadDbHelper(context).deleteDownloadingInfo();
-//    }
-
     public static void appCreateSAAS(final Context context, String appKey, String appSecret) {
         App.setContext(context);
         Config.SDK_VERSION = BuildConfig.SDK_VERSION;
@@ -60,7 +32,7 @@ public class VideoPlus {
         if (!TextUtils.isEmpty(appKey) && !TextUtils.isEmpty(appSecret)) {
             PlatformInfo platformInfo = new PlatformInfo.Builder().setAppKey(appKey).setAppSecret(appSecret).builder();
             Platform platform = new Platform(platformInfo);
-            new VideoPlusLuaUpdateModel(platform, null).startRequest();
+            new VideoPlusPreloadLuaFileInfo(platform,null).startRequest();
         }
         VenvyRouterManager.getInstance().init(context, DebugStatus.isRelease() ? null : new VenvyRouterManager.RouterInitResult() {
             @Override
