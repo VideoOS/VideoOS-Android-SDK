e254c0bd041e50ca4cb12f9d4f6eb05878544272
bolo <ppcc0019@gmail.com>
2019-11-27 11:24:06 +0800

    upload to fir

--- VideoOS/auto_assemble.sh
+++ VideoOS/auto_assemble.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+TOKEN="45575a29a8a6ab766ebb0ae78879da23"
+SHORT="AndroidDevApp"
+
 cd videoos_dev_app
 gradle clean assembleRelease
+
+fir publish --short=$SHORT --token=$TOKEN ./build/outputs/apk/videoos_dev_app-release.apk
