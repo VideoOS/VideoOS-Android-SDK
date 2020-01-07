80a90e45d3e85436131ffd9ab8e7dec839856a65
lucas <liangzifeng@videopls.com>
2020-01-07 15:44:57 +0800

    fix 明星百科动画不执行的bug

--- VideoOS/venvy_pub/src/main/java/cn/com/videopls/pub/view/VideoOSLuaView.java
+++ VideoOS/venvy_pub/src/main/java/cn/com/videopls/pub/view/VideoOSLuaView.java
@@ -216,7 +216,7 @@ public class VideoOSLuaView extends VideoOSBaseView {
         if (TextUtils.isEmpty(luaName)) {
             return;
         }
-//        if(luaName.contains("os_desktop_hotspot")){
+//        if (luaName.contains("os_video_starContent_hotspot") || luaName.contains("os_video_figureStarList_hotspot")) {
 //            runLua(luaView, luaName, valueData);
 //            return;
 //        }
