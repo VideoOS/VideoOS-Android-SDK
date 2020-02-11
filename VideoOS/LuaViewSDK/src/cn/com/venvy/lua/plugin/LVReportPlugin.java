7f48770ea13a0aef11588b327c719d9bbfd4752c
androidlgf <android_lgf@163.com>
2020-02-11 17:24:26 +0800

    添加Lua 日志上报LVReportPlugin类

--- VideoOS/LuaViewSDK/src/cn/com/venvy/lua/plugin/LVReportPlugin.java
+++ VideoOS/LuaViewSDK/src/cn/com/venvy/lua/plugin/LVReportPlugin.java
@@ -8,6 +8,7 @@ import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.VarArgFunction;
 
+import cn.com.venvy.common.report.Report;
 import cn.com.venvy.lua.binder.VenvyLVLibBinder;
 
 /**
@@ -18,6 +19,7 @@ import cn.com.venvy.lua.binder.VenvyLVLibBinder;
 public class LVReportPlugin {
 
     private static LuaReport sLuaReport;
+    private static final String TAG = "LVReportPlugin";
 
     public static void install(VenvyLVLibBinder venvyLVLibBinder) {
         venvyLVLibBinder.set("report", sLuaReport == null ? sLuaReport = new LuaReport() : sLuaReport);
@@ -28,17 +30,26 @@ public class LVReportPlugin {
         public Varargs invoke(Varargs args) {
             int fixIndex = VenvyLVLibBinder.fixIndex(args);
             if (args.narg() > fixIndex) {
-
+                LuaValue keyValue = args.arg(fixIndex + 2);  //key
+                String message = VenvyLVLibBinder.luaValueToString(keyValue);
+                if (TextUtils.isEmpty(message)) {
+                    return LuaValue.NIL;
+                }
                 switch (LuaUtil.getString(args, fixIndex + 1)) {
                     case "0":
+                        Report.report(Report.ReportLevel.i, TAG, message);
                         break;
                     case "1":
+                        Report.report(Report.ReportLevel.w, TAG, message);
                         break;
                     case "2":
+                        Report.report(Report.ReportLevel.e, TAG, message);
                         break;
                     case "3":
+                        Report.report(Report.ReportLevel.u, TAG, message);
                         break;
                     case "4":
+                        Report.report(Report.ReportLevel.d, TAG, message);
                         break;
                     default:
                         break;
